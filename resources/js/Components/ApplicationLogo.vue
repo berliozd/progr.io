91a8705a5aa230b54a41f7b1767c571840de75dd
Didier Berlioz <berliozd@gmail.com>
2024-03-05 13:37:11 +0100

    fix logo margin top

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -5,7 +5,7 @@ const page = usePage();
 const appName = page.props.app.name;
 </script>
 <template>
-    <div class="flex flex-col border h-auto rounded p-1">
+    <div class="mt-1 flex flex-col border h-auto rounded p-1">
         <img src="/img/icon.png" width="30px" height="30px" v-bind:alt="appName" v-bind:title="appName">
         {{ appName }}
     </div>
