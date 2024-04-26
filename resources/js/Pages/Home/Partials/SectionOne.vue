06962c175b28e63f4166d665a6d250a36b58f8d1
Didier Berlioz <berliozd@gmail.com>
2024-04-26 12:08:20 +0200

    Add video on HP

--- resources/js/Pages/Home/Partials/SectionOne.vue
+++ resources/js/Pages/Home/Partials/SectionOne.vue
@@ -7,43 +7,43 @@ import {router} from "@inertiajs/vue3";
 </script>
 
 <template>
-  <section class="md:p-12 p-6 mx-auto">
-    <div class="px-8 pb-6 flex flex-col gap-10 lg:gap-14 items-center text-center">
-      <h1 class="md:text-7xl text-3xl my-6 font-extrabold leading-normal">
-        Turn your
-        <span class="border-b-8 border-dashed border-primary/50 italic">ideas</span>
-        <br>
-        into
-        <span class="border-b-8 border-dashed border-primary/50 italic">successful projects</span>
-      </h1>
-<!--      <div class="text-lg max-w-xl mx-auto">-->
-<!--        Progr.io is the tool that helps you keep track of your ideas and turn them into successful projects.-->
-<!--        <br>-->
-<!--        You can easily follow the progress of your ideas, collaborate with your team, get help from AI, and stay-->
-<!--        organized.-->
-<!--        <br>-->
-<!--        <br>-->
-<!--        Don't let your ideas slip away - track them and turn them into successful projects.-->
-<!--      </div>-->
-      <div  class="text-lg max-w-xl mx-auto">
-        Our AI-powered tool not only helps you track progress and collaborate, but also provides valuable insights and information on your project's potential, including competitor analysis.
-        <br>
-        Save time and make informed decisions with Progr.io.
-      </div>
-      <ul class="space-y-1.5">
-        <li class="flex items-center justify-start gap-2">
-          <ListTick>Start for free</ListTick>
-        </li>
-        <li class="flex items-center justify-start gap-2">
-          <ListTick>Track your projects</ListTick>
-        </li>
-        <li class="flex items-center justify-start gap-2">
-          <ListTick>Get help from AI</ListTick>
-        </li>
-      </ul>
-      <PrimaryButton @click="router.visit('register')">
-        <Arrow>Get started</Arrow>
-      </PrimaryButton>
-    </div>
-  </section>
+    <section class="md:p-12 p-6 mx-auto">
+        <div class="px-8 pb-6 flex flex-col gap-10 lg:gap-14 items-center text-center">
+            <h1 class="md:text-7xl text-3xl my-6 font-extrabold leading-normal">
+                Turn your
+                <span class="border-b-8 border-dashed border-primary/50 italic">ideas</span>
+                <br>
+                into
+                <span class="border-b-8 border-dashed border-primary/50 italic">successful projects</span>
+            </h1>
+            <div class="text-lg max-w-xl mx-auto">
+                Our AI-powered tool not only helps you track progress and collaborate, but also provides valuable
+                insights and information on your project's potential, including competitor analysis.
+                <br>
+                Save time and make informed decisions with Progr.io.
+            </div>
+            <ul class="space-y-1.5">
+                <li class="flex items-center justify-start gap-2">
+                    <ListTick>Start for free</ListTick>
+                </li>
+                <li class="flex items-center justify-start gap-2">
+                    <ListTick>Track your projects</ListTick>
+                </li>
+                <li class="flex items-center justify-start gap-2">
+                    <ListTick>Get help from AI</ListTick>
+                </li>
+            </ul>
+            <PrimaryButton @click="router.visit('register')">
+                <Arrow>Get started</Arrow>
+            </PrimaryButton>
+
+            <div class="aspect-video w-full sm:w-1/2">
+                <iframe class="w-full h-full"
+                        src="https://www.youtube.com/embed/r-fareAicFk?si=uBYDp3VdunwXZsd6&amp;controls=0"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
 </template>
--- resources/js/Pages/Home/Partials/SectionSix.vue
+++ resources/js/Pages/Home/Partials/SectionSix.vue
@@ -8,7 +8,7 @@ import {router} from "@inertiajs/vue3";
   <section class=" text-center md:p-12 p-6 bg-neutral">
     <div class="max-w-3xl bg-base-100 mx-auto rounded-box">
       <div class="flex flex-col items-center p-12 md:p-16">
-        <h2 class="text-3xl md:text-5xl mb-8 md:mb-12">Track your ideas</h2>
+        <h2 class="text-3xl md:text-5xl mb-8 md:mb-12">Track your ideas and find new ones</h2>
         <p class="text-lg text-base-content-secondary mb-12 md:mb-16">
           Don't let your ideas escape you: follow them, get help from AI to define them and transform them into
           successful projects.
