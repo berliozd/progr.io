1913fd2e435cd7eb5e33f6289f901d708952107e
Didier Berlioz <berliozd@gmail.com>
2024-05-15 17:31:01 +0200

    Update sitemap generation

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -6,61 +6,21 @@ use App\Models\Category;
 use App\Models\Project;
 use App\Models\ProjectsVisibility;
 use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\SitemapGenerator;
 
 class GenerateSitemap extends Command
 {
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
     protected $signature = 'sitemap:generate';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Generate the sitemap.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        // modify this to your own needs
         \Log::info('Generating sitemap');
-
         $sitemap = SitemapGenerator::create(config('app.url'))->getSitemap();
-
-        $categories = Category::all();
-        foreach ($categories as $category) {
-            $sitemap->add(route('app.ideas.catalog.category', ['category' => $category->code]));
-            $projectIdeas = Project::where('visibility', ProjectsVisibility::where('code', 'public')->first()->id)
-                ->where('category_id', Category::where('code', $category->code)->first()->id)
-                ->whereNotNull('category_id')
-                ->with('category')
-                ->orderBy('updated_at', 'desc')
-                ->get();
-            foreach ($projectIdeas as $projectIdea) {
-                $sitemap->add(
-                    route(
-                        'app.ideas.catalog.idea',
-                        [
-                            'category' => $category->code,
-                            'title' => $this->formatTitle($projectIdea->title),
-                            'id' => $projectIdea->id
-                        ]
-                    )
-                );
-            }
-        }
-
+        $this->addCategories($sitemap);
         $sitemap->writeToFile(public_path('sitemap.xml'));
-
         \Log::info('Sitemap Generated');
     }
 
@@ -68,5 +28,41 @@ class GenerateSitemap extends Command
     {
         $title = preg_replace('/[^A-Za-z0-9]+/', '-', strtolower($title));
         return trim($title, '-');
+    }
+
+    public function addCategories(Sitemap $sitemap): void
+    {
+        $categories = Category::all();
+        foreach ($categories as $category) {
+            $sitemap->add(route('app.ideas.catalog.category', ['category' => $category->code]));
+            $this->addCategoryProjectIdeas($sitemap, $category);
+        }
+    }
+
+    public function addProjectIdea(Sitemap $sitemap, Category $category, mixed $projectIdea): void
+    {
+        $sitemap->add(
+            route(
+                'app.ideas.catalog.idea',
+                [
+                    'category' => $category->code,
+                    'title' => $this->formatTitle($projectIdea->title),
+                    'id' => $projectIdea->id
+                ]
+            )
+        );
+    }
+
+    public function addCategoryProjectIdeas(Sitemap $sitemap, Category $category): void
+    {
+        $projectIdeas = Project::where('visibility', ProjectsVisibility::where('code', 'public')->first()->id)
+            ->where('category_id', Category::where('code', $category->code)->first()->id)
+            ->whereNotNull('category_id')
+            ->with('category')
+            ->orderBy('updated_at', 'desc')
+            ->get();
+        foreach ($projectIdeas as $projectIdea) {
+            $this->addProjectIdea($sitemap, $category, $projectIdea);
+        }
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('sitemap:generate')->twiceDaily();
+        $schedule->command('sitemap:generate')->everyFourHours();
         $schedule->command('project:auto_populate_projects')->everyFiveMinutes();
         $schedule->command('project:enrich_projects')->everyFiveMinutes();
     }
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
