18c39d7836fceb40bf3e32b8f05784fe9251a596
Didier Berlioz <berliozd@gmail.com>
2024-05-21 18:31:19 +0200

    Remove ad on HP

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,19 +46,5 @@
 <script async src="https://cdn.promotekit.com/promotekit.js"
         data-promotekit="{{config('services.promotekit.api_key')}}"></script>
 @inertia
-<div class="flex justify-center">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0882802698917065"
-            crossorigin="anonymous"></script>
-    <!-- progr.io -->
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-0882802698917065"
-         data-ad-slot="3474737115"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-</div>
 </body>
 </html>
