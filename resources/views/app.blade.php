628149c052bfc0e7b769f9ded7dffbb50f384733
Didier Berlioz <berliozd@gmail.com>
2025-01-22 16:20:13 +0100

    remove ad on project auth pages

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,6 @@
     <title inertia>{{ config('app.name', 'Saas boilerplate') }}</title>
     <link rel="icon" sizes="64x64" href="{{ asset('img/icon.png') }}" type="image/x-icon">
     @include('components.partials.gtag')
-    @include('components.partials.ad-site')
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
